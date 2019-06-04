98ca6b9e32862328cb1a9bb9c973cf248b40a898
Lichthagel <lichthagel@tuta.io>
2019-06-04 16:49:53 +0200

    remove .idea

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -124,7 +124,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/data/Wallpapers/goblin_slayer_by_brunowc_dcqr66l_[NS-L1][x6.000000].png"
+theme.wallpaper = "WALLPAPER"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
